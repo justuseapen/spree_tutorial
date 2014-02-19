d2e7009d475c0a955f30a0cf1673abc48126f799
Justus Daniel Eapen Jr <justuseapen@gmail.com>
2014-02-19 15:45:44 +0000

    Initial Commit, made small spree app and started extension

---
+++ mystore/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '4b599d222bbbd729769f8e0ce5a4208399751acfed2677c09bd6259b45acedbcf26c93c2c1e6b85cec6d18f7eb866e144eab440e8f836bc1cbb7c11c4b2bf56b'
